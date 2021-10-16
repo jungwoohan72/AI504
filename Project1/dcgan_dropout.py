dc8e245c553111188b606159aa48e1ae6ed70ca9
Jungwoo <jungwoohan72@kaist.ac.kr>
2021-10-16 16:15:50 +0900

    AI504

--- Project1/dcgan_dropout.py
+++ Project1/dcgan_dropout.py
@@ -9,22 +9,18 @@ class Generator(nn.Module):
             nn.ConvTranspose2d(input_dim, gen_filter * 8, 4, 1, 0, bias=False),
             nn.BatchNorm2d(gen_filter * 8),
             nn.ReLU(True),
-            nn.Dropout(p=0.5),
             # state size. (ngf*8) x 4 x 4
             nn.ConvTranspose2d(gen_filter * 8, gen_filter * 4, 4, 2, 1, bias=False),
             nn.BatchNorm2d(gen_filter * 4),
             nn.ReLU(True),
-            nn.Dropout(p=0.5),
             # state size. (ngf*4) x 8 x 8
             nn.ConvTranspose2d( gen_filter * 4, gen_filter * 2, 4, 2, 1, bias=False),
             nn.BatchNorm2d(gen_filter * 2),
             nn.ReLU(True),
-            nn.Dropout(p=0.5),
             # state size. (ngf*2) x 16 x 16
             nn.ConvTranspose2d( gen_filter * 2, gen_filter, 4, 2, 1, bias=False),
             nn.BatchNorm2d(gen_filter),
             nn.ReLU(True),
-            nn.Dropout(p=0.5),
             # state size. (ngf) x 32 x 32
             nn.ConvTranspose2d( gen_filter, num_channel, 4, 2, 1, bias=False),
             nn.Tanh()
@@ -40,24 +36,23 @@ class Discriminator(nn.Module):
         self.main = nn.Sequential(
             # input is (nc) x 64 x 64
             nn.Conv2d(input_dim, dis_filter, 4, 2, 1, bias=False),
-            nn.InstanceNorm2d(dis_filter, affine=True),
             nn.LeakyReLU(0.2, inplace=True),
-            nn.Dropout(p=0.5),
+            nn.Dropout(p=0.25),
             # state size. (ndf) x 32 x 32
             nn.Conv2d(dis_filter, dis_filter * 2, 4, 2, 1, bias=False),
-            nn.InstanceNorm2d(dis_filter * 2, affine=True),
+            nn.BatchNorm2d(dis_filter * 2),
             nn.LeakyReLU(0.2, inplace=True),
-            nn.Dropout(p=0.5),
+            nn.Dropout(p=0.25),
             # state size. (ndf*2) x 16 x 16
             nn.Conv2d(dis_filter * 2, dis_filter * 4, 4, 2, 1, bias=False),
-            nn.InstanceNorm2d(dis_filter * 4, affine=True),
+            nn.BatchNorm2d(dis_filter * 4),
             nn.LeakyReLU(0.2, inplace=True),
-            nn.Dropout(p=0.5),
+            nn.Dropout(p=0.25),
             # state size. (ndf*4) x 8 x 8
             nn.Conv2d(dis_filter * 4, dis_filter * 8, 4, 2, 1, bias=False),
-            nn.InstanceNorm2d(dis_filter * 8, affine=True),
+            nn.BatchNorm2d(dis_filter * 8),
             nn.LeakyReLU(0.2, inplace=True),
-            nn.Dropout(p=0.5),
+            nn.Dropout(p=0.25),
             # state size. (ndf*8) x 4 x 4
             nn.Conv2d(dis_filter * 8, 1, 4, 1, 0, bias=False),
             nn.Sigmoid()
